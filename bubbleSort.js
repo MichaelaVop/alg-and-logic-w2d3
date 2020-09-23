428020b84a0e7a3d38f1f179d6cfffd914d00281
GilOnGithub <gmjetomo@outlook.com>
2020-09-23 10:35:06 -0700

    completed bubbleSort

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,30 +1,31 @@
 //by Koji, Michaela, and Gil
 
-let nums = [7, 5, 9, 4, 1, 8, 25, 3];
+let nums = [5, 7, 9, 4, 1, 8, 25, 3];
 let otherNums = [46, 2, 34, 90, 56, 27];
 let finalTest = [5, 7, 98, 36, 47, 45, 5, 2];
 
 const bubbleSort = function(numbers) {
+let temp = 0;
+let swapCounter = -1;
 
-    let swapCounter = -1;
+console.log('Original array ', numbers);
 
-    console.log(numbers);
-    // while(swapCounter != 0) {
+    while(swapCounter != 0) {
         
-        for(i = 0; i < numbers.length; i++) {
+        swapCounter = 0;
 
-               
+        for(i = 0; i < numbers.length; i++) {
             if(numbers[i] > numbers[i+1]){
                 swapCounter += 1;
                 temp = numbers[i];
                 numbers[i] = numbers[i+1];
                 numbers[i+1] = temp;
             }
+        }
 
-    console.log(numbers);
-
-        // }
     }
+
+return `Sorted array ${numbers}`;
 
 }
 
@@ -33,5 +34,5 @@ const bubbleSort = function(numbers) {
 //if two adjacent elements are not in order, swap the elements and add 1 to swap counter
 
 console.log(bubbleSort(nums));
-// console.log(bubbleSort(otherNums));
-// console.log(bubbleSort(finalTest));
+console.log(bubbleSort(otherNums));
+console.log(bubbleSort(finalTest));
