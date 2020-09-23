3ab9ee53ef1ee3065220da93bd2ee830f8e773ec
wanko <kouji124@w4.dion.ne.jp>
2020-09-24 04:30:44 +0900

    completed

--- selectionSort.js
+++ selectionSort.js
@@ -1,37 +1,43 @@
 //by Koji, Michaela, and Gil
 
-let nums = [5, 7, 9, 4, 1, 8, 25, 3];
+let nums = [5, 7, 9, 4, 1, 8, 25, 3, 1, 3, 9];
 let otherNums = [46, 2, 34, 90, 56, 27];
 let finalTest = [5, 7, 98, 36, 47, 45, 5, 2];
 
 const selectionSort = function(numbers) {
+
 let counter = 0;
 let min = numbers[counter];
+let j = 0;
 
-    console.log(numbers);
+    // console.log(numbers);
 
-while (counter < numbers.length-1) {
+while (counter < 3) {
     for(i = counter; i < numbers.length; i++) {
-        if(numbers[i] < min){
+        if(numbers[i] <= min){
             min = numbers[i];
+            j = i;
         }
     }
+    // console.log(j);
 
-    numbers[numbers.indexOf(min)] = numbers[counter];
+    numbers[j] = numbers[counter];
     numbers[counter] = min;
     counter += 1;
     min = numbers[counter];
 
-console.log(numbers);
+// console.log(numbers);
+
 }
 
 
  //  console.log(counter);
+ return numbers;
 
 }
 
 //find the smallest element in the array and move it to the left side of the array. (swap)
 
 console.log(selectionSort(nums));
-// console.log(selectionSort(otherNums));
-// console.log(selectionSort(finalTest));
+console.log(selectionSort(otherNums));
+console.log(selectionSort(finalTest));
