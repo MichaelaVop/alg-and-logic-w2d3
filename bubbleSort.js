d67c4bf8cd2cc72a18369a1e4e78e00e85ffcd5b
GilOnGithub <gmjetomo@outlook.com>
2020-09-23 09:44:54 -0700

    first commit

--- bubbleSort.js
+++ bubbleSort.js
@@ -1,1 +1,13 @@
 //by Koji, Michaela, and Gil
+
+let nums = [5, 7, 9, 4, 1, 8, 25, 3];
+let otherNums = [46, 2, 34, 90, 56, 27];
+let finalTest = [5, 7, 98, 36, 47, 45, 5, 2];
+
+const bubbleSort = function(numbers) {
+
+}
+
+console.log(bubbleSort(nums));
+console.log(bubbleSort(otherNums));
+console.log(bubbleSort(finalTest));
